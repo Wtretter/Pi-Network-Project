f8f973483a2b9cf3a1b6f9a174ffff77c041148b
William Tretter <will@wtretter.com>
2025-06-29 17:11:31 -0500

    Started on DNS request blocking

--- dns-checker.py
+++ dns-checker.py
@@ -2,16 +2,95 @@
 
 import socket
 import os
+from scapy.layers.l2 import Ether
+from scapy.layers.dns import DNS, EDNS0TLV
+
+
+log_file = open("dns.log", "a")
+
+with open("blacklist", "r") as blacklist_file:
+    blacklist = blacklist_file.read().splitlines()
 
 def handle_client(client: socket.socket, address):
     while True:
+        modified = False
         packet = client.recv(8192)
 
-        
+        packet, ident = packet[:-4], packet[-4:]
+    
+        parsed_packet = Ether(packet)
+        dns_message = parsed_packet.lastlayer()
+        if isinstance(dns_message, DNS):
+            req_domain = dns_message.fields["qd"].qname.decode()[:-1]
 
+            if not (dns_message.fields["qr"]):
+                if req_domain in blacklist:
+                    log_file.write(req_domain+" (BLOCKED)\n")
+                    log_file.flush()
+                    
+                else:    
+                    log_file.write(req_domain+"\n")
+                    log_file.flush()
+            else:
+                if req_domain in blacklist:
+                    remove_dns_answers(parsed_packet)
+                    modified = True
+                    
+        else:
+            print("Packet not DNS")
+
+        # parsed_packet.show()
+
+
+        # print(dns_message.fields)
+
+        if modified:
+            packet = parsed_packet.build()
+            eth_header = packet[:14]
+            next_proto = int.from_bytes(packet[12:14])
+            if next_proto == 0x0800:
+                # IPV4
+                ipv4_len = int.from_bytes(packet[16:18])
+                pre_len = packet[14:16]
+                rest_of_packet = packet[18:]
+                correct_ip_len = len(packet) - 14
+                if ipv4_len != correct_ip_len:
+                    print("IPV4 len incorrect, fixing")
+                    packet = eth_header + pre_len + correct_ip_len.to_bytes(2) + rest_of_packet
+                    ihl = (packet[14] & 0b00001111) * 4
+                    udp_header_start = 14 + ihl
+                    packet_b4_udp_len = packet[:udp_header_start+4]
+                    packet_after_udp_len = packet[udp_header_start+6:]
+                    udp_len = int.from_bytes(packet[udp_header_start+4:udp_header_start+6])
+                    correct_udp_len = len(packet) - udp_header_start
+                    if udp_len != correct_udp_len:
+                        packet = packet_b4_udp_len + correct_udp_len.to_bytes(2) + packet_after_udp_len
+
+
+            elif next_proto == 0x86DD:
+                # IPV6
+                pass
+            else:
+                print("Invalid Next Protocol")
+        parsed_packet = Ether(packet)
+        
+        # print(packet.hex(bytes_per_sep=1, sep=" "), "\n")
+        parsed_packet.show()
+        packet += ident
         client.send(packet)
         
 
+def remove_dns_answers(parsed_packet: Ether):
+    dns_message: DNS = parsed_packet.lastlayer()
+    dns_message.fields["ancount"] = 0
+    dns_message.fields["an"] = None
+    dns_message.fields["rcode"] = 5
+    dns_message.fields["aa"] = 0
+
+    dns_message.fields["ar"].fields["rdata"] = [EDNS0TLV(optcode=15, optlen=2, optdata='\x00\x17')]
+    dns_message.fields["ar"].fields["rdlen"] = 6
+
+    
 def main():
     try:
         os.unlink("./packet.sock")
